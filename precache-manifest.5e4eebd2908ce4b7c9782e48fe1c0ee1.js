1a7b8e8ecf2aafdd99de2f25cd7234f34015429e
milexys96 <milexysferrer@gmail.com>
2020-05-28 23:44:50 -0400

    Updates

---
+++ precache-manifest.5e4eebd2908ce4b7c9782e48fe1c0ee1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a167430b193a6a4a90463ebde4a1b9",
+    "url": "/Milexys/Desafio-cumplo/index.html"
+  },
+  {
+    "revision": "4741d8139792c6ce318c",
+    "url": "/Milexys/Desafio-cumplo/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "fa987615a9d346997b7d",
+    "url": "/Milexys/Desafio-cumplo/static/css/main.dd818f4a.chunk.css"
+  },
+  {
+    "revision": "4741d8139792c6ce318c",
+    "url": "/Milexys/Desafio-cumplo/static/js/2.134a6368.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/Milexys/Desafio-cumplo/static/js/2.134a6368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa987615a9d346997b7d",
+    "url": "/Milexys/Desafio-cumplo/static/js/main.e5929102.chunk.js"
+  },
+  {
+    "revision": "6d14fb33c68d6ae2447a",
+    "url": "/Milexys/Desafio-cumplo/static/js/runtime-main.cac3c872.js"
+  }
+]);
